9b9a428b820fbbdcb512f06b5738e45c2bd476ba
Irina Adamuk <grinirina032@gmail.com>
2022-08-29 19:32:08 +0300

    second task was done

--- cli.js
+++ cli.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
-const { stdin } = require("process");
+const { stdin, exit } = require("process");
 const readline = require("readline");
 // const yargs = require("yargs");
 const path = require('path');
 const inquirer = require('inquirer');
 
-const executionDir = process.cwd();
+
 
 // console.log(process.argv);
 
@@ -60,28 +60,75 @@ const rl = readline.createInterface({
 
 
 
+
+
 const fileFilter = (fileOrDir) => fs.lstatSync(fileOrDir).isFile();
-const list = fs.readdirSync(executionDir).filter(fileFilter);
+// const list = fs.readdirSync(executionDir).filter(fileFilter);
+let executionDir = process.cwd();
+const MESSAGE_CHOOSE_PATH = 'Выбрать свой путь';
 
-// console.log(list);
 
-inquirer
-  .prompt([
-    {
-      name: "fileName",
-      type: "list", // input, number, confirm, list, checkbox, password
-      message: "Выбирите файл для чтения",
-      choices: list,
-    },
-  ])
-  .then(({ fileName }) => {
+const getList = (dirPath) => {
+  let l = fs.readdirSync(dirPath)
+  l.unshift(MESSAGE_CHOOSE_PATH)
+  // console.log(l); exit();
+  return l
+}
 
-    const fullFilePath = path.join(executionDir, fileName);
+let list = getList(executionDir);
 
+const showPathContent = (fullFilePath) => {
+  if (fs.lstatSync(fullFilePath).isDirectory()) {
+    list = fs.readdirSync(fullFilePath);
+    executionDir = fullFilePath;
+    askQuestion();
+  } else {
+    console.log("FullFilePath ---> ", fullFilePath);
     fs.readFile(fullFilePath, 'utf8', (err, data) => {
       if (err) {
         console.log(err);
       }
       console.log(data);
     });
+  }
+}
+
+const askQuestion = () => {
+  inquirer
+  .prompt([
+    {
+      name: "fileName",
+      type: "list", // input, number, confirm, list, checkbox, password
+      message: "Выбeрите файл для чтения",
+      choices: list,
+    },
+  ])
+  .then(({ fileName }) => {
+    if (fileName == MESSAGE_CHOOSE_PATH) {
+      inquirer
+      .prompt([
+        {
+          name: "promptPath",
+          type: "input", // input, number, confirm, list, checkbox, password
+          message: "Введите путь до файла/папка",
+        },
+      ])
+      .then(({promptPath}) => {
+        if (fs.existsSync(promptPath)) {
+          showPathContent(promptPath)
+        } else {
+          console.log('Wrong path', promptPath)
+          exit()
+        }
+        
+      })
+    } else {
+      const fullFilePath = path.join(executionDir, fileName);
+      showPathContent(fullFilePath)
+    }
   });
+}
+
+askQuestion();
+
+
